6783f9278854beffdf6964d3159ecb8edbd81c5f
Aprovvv <risilvin@gmail.com>
2024-08-15 16:36:37 +1000

    Исправил сравнение даблов и прочие мелочи

--- main.c
+++ main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <stdbool.h>
-#include <locale.h>
+#include <TXLib.h>
 
 #define INF_ROOTS -1 //возвращаемое значение solve_equation, если корней бесконечно много
 #define ROOT_SING_COUNT 3 //число знаков после запятой при выводе корней
@@ -11,20 +10,20 @@
 int solve_equation(double a, double b, double c, double *x1, double *x2);//функция, которая решает уравнение и возвращает количество корней
 void print_menu(void);//функция которая выдает приглашение на ввод
 void print_roots(int count, double x1, double x2);//функция которая печатает корни
-bool eqdoubles(double a, double b, double eps);//функция для сравнения даблов с заданной точностью
+int cmpdoubles(double a, double b, double eps);//функция для сравнения даблов с заданной точностью
 
 int main()
 {
     double a_coef=0, b_coef=0, c_coef=0;
     double x1 = 0, x2 = 0;
     int count = 0;
-
-    setlocale(LC_ALL, "RUS");
+    char ch;
 
     print_menu();
 
     while(scanf("%lf %lf %lf", &a_coef, &b_coef, &c_coef) == 3)
     {
+        printf("%f %f %f", a_coef, b_coef, c_coef);
         count = solve_equation(a_coef, b_coef, c_coef, &x1, &x2);
         print_roots(count, x1, x2);
         print_menu();
@@ -45,11 +44,11 @@ int solve_equation(double a, double b, double c, double *x1_adress, double *x2_a
      if (D<0)
         return 0;
      //случай нулевых коэффициентов
-     if (eqdoubles(a, 0, EPS_COEF))
+     if (!cmpdoubles(a, 0, EPS_COEF))
      {
-        if (eqdoubles(b, 0, EPS_COEF))
+        if (!cmpdoubles(b, 0, EPS_COEF))
         {
-            if (eqdoubles(c, 0, EPS_COEF))
+            if (!cmpdoubles(c, 0, EPS_COEF))
                 return INF_ROOTS;
             else
                 return 0;
@@ -61,7 +60,7 @@ int solve_equation(double a, double b, double c, double *x1_adress, double *x2_a
         }
      }
      //случай 1 корень
-     if (eqdoubles(sqrtD/a, 0, pow(10, -ROOT_SING_COUNT)))//пренебрегаем разницей между корнями порядка 10^-ROOT_SING_COUNT, т.к. выводим с такой точностью
+     if (!cmpdoubles(sqrtD/a, 0, pow(10, -ROOT_SING_COUNT)))//пренебрегаем разницей между корнями порядка 10^-ROOT_SING_COUNT, т.к. выводим с такой точностью
      {
         *x1_adress = -b/(2*a);
         return 1;
@@ -103,7 +102,14 @@ void print_roots(int count, double x1, double x2)//функция которая
     }
 }
 
-bool eqdoubles(double a, double b, double eps)//функция для сравнения даблов с заданной точностью
+//функция для сравнения даблов с заданной точностью
+//-1 = a<b; 0 = a=b; +1 = a>b
+int cmpdoubles(double a, double b, double eps)
 {
-    return (fabs(a-b)<eps);
+    if (fabs(a-b)<eps)
+        return 0;
+    if (a<b)
+        return -1;
+    else
+        return 1;
 }
